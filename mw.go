714b771b24dc1740dec1501920263381b2844617
Oleg Ivanov <morhekil@morhekil.net>
2015-02-01 22:27:31 +1100

    updated docs

---
+++ mw.go
@@ -0,0 +1,9 @@
+/*
+Package mw is a collection of various Go middlewares. For details
+about any non-trivial middlewares, refer to the documentation
+of the corresponding sub-package.
+
+Feel free to open issues and/or features requests on project's GitHub
+page at https://github.com/morhekil/mw
+*/
+package mw
---
+++ mw_test.go
@@ -0,0 +1,27 @@
+package mw_test
+
+import (
+	"net/http"
+
+	"github.com/justinas/alice"
+	"github.com/morhekil/mw"
+)
+
+func Example() {
+	// Sample application that combines all included
+	// middlewares together into a single stack
+	app := http.NotFoundHandler()
+
+	hs := map[string]string{
+		"Content-Type": "application/json; charset=utf-8",
+	}
+
+	a := alice.New(
+		mw.Recover,
+		mw.Logger,
+		mw.Chaotic("/chaotic"),
+		mw.Headers(hs),
+	).Then(app)
+
+	http.ListenAndServe(":1234", a)
+}
